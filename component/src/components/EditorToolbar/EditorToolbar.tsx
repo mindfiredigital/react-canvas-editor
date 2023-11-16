b04fa2be5a6076d509acc8d2ee09f71449545189
Vershala Tandon <vershalatandon@gmail.com>
2023-11-16 17:08:49 +0530

    feat: update toolbar styling.

--- component/src/components/EditorToolbar/EditorToolbar.tsx
+++ component/src/components/EditorToolbar/EditorToolbar.tsx
@@ -33,12 +33,16 @@ import HeadingButton from "../HeadingButton/HeadingButton";
 import { IRangeStyle } from "@mindfiredigital/canvas-editor/dist/src/editor/interface/Listener";
 import InsertLinkIcon from "@mui/icons-material/InsertLink";
 import ImageUploadButton from "../ImageUploadButton/ImageUploadButton";
+import Divider from "@mui/material/Divider";
 
 interface content {
-  toolbar: any,
-  toolbarClass: any
+  toolbar: any;
+  toolbarClass: any;
 }
-const EditorToolbar = forwardRef<HTMLDivElement,content>(function Toolbar(_props, ref) {
+const EditorToolbar = forwardRef<HTMLDivElement, content>(function Toolbar(
+  _props,
+  ref
+) {
   const [contentStyles, setContentStyles] = useState<IRangeStyle | undefined>();
   useEffect(() => {
     let interval: NodeJS.Timeout;
@@ -57,118 +61,228 @@ const EditorToolbar = forwardRef<HTMLDivElement,content>(function Toolbar(_props
 
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar
-        position="fixed"
-        sx={_props?.toolbarClass?.container}
-      >
-        <Stack
-          sx={_props?.toolbarClass?.primaryToolbar}
-        >
-          {(!_props?.toolbar || _props?.toolbar?.undo) && <ButtonWrapper sx={_props?.toolbarClass?.item?.undo} title="undo" handleClick={DOMEventHandlers.handleUndo}>
-            <UndoIcon />
-          </ButtonWrapper>}
+      <AppBar position='fixed' sx={_props?.toolbarClass?.container}>
+        <Stack sx={_props?.toolbarClass?.primaryToolbar}>
+          {(!_props?.toolbar || _props?.toolbar?.undo) && (
+            <ButtonWrapper
+              sx={_props?.toolbarClass?.item?.undo}
+              title='Undo'
+              handleClick={DOMEventHandlers.handleUndo}>
+              <UndoIcon style={{ fontSize: "large" }} />
+            </ButtonWrapper>
+          )}
+          {(!_props?.toolbar || _props?.toolbar?.redo) && (
+            <ButtonWrapper
+              sx={_props?.toolbarClass?.item?.redo}
+              title='Redo'
+              handleClick={DOMEventHandlers.handleRedo}>
+              <RedoIcon style={{ fontSize: "large" }} />
+            </ButtonWrapper>
+          )}
+          <Divider
+            flexItem
+            orientation='vertical'
+            sx={{ mx: 0.5, my: 1 }}
+            style={{ marginLeft: -5, marginRight: 5 }}
+          />
 
-          {(!_props?.toolbar || _props?.toolbar?.redo) && <ButtonWrapper sx={_props?.toolbarClass?.item?.redo} title="redo" handleClick={DOMEventHandlers.handleRedo}>
-            <RedoIcon />
-          </ButtonWrapper>}
-          {(!_props?.toolbar || _props?.toolbar?.bold) && <ButtonWrapper sx={_props?.toolbarClass?.item?.bold} title="bold" handleClick={DOMEventHandlers.handleBold}>
-            <FormatBoldIcon />
-          </ButtonWrapper>}
-          {(!_props?.toolbar || _props?.toolbar?.italic) && <ButtonWrapper
-           sx={_props?.toolbarClass?.item?.italic} 
-            title="italic"
-            handleClick={DOMEventHandlers.handleItalic}
-          >
-            <FormatItalicIcon />
-          </ButtonWrapper>}
-          {(!_props?.toolbar || _props?.toolbar?.underline) && <ButtonWrapper
-            sx={_props?.toolbarClass?.item?.underline} 
-            title="underline"
-            handleClick={DOMEventHandlers.handleUnderline}
-          >
-            <FormatUnderlinedIcon />
-          </ButtonWrapper>}
-          {(!_props?.toolbar || _props?.toolbar?.subscript) && <ButtonWrapper
-            sx={_props?.toolbarClass?.item?.subscript} 
-            title="subscript"
-            handleClick={DOMEventHandlers.handleSubscript}
-          >
-            <SubscriptIcon />
-          </ButtonWrapper>}
-          {(!_props?.toolbar || _props?.toolbar?.superscript) && <ButtonWrapper
-            sx={_props?.toolbarClass?.item?.superscript} 
-            title="superscript"
-            handleClick={DOMEventHandlers.handleSuperscript}
-          >
-            <SuperscriptIcon />
-          </ButtonWrapper>}
-          {(!_props?.toolbar || _props?.toolbar?.strikethrough) && <ButtonWrapper
-            sx={_props?.toolbarClass?.item?.strikethrough} 
-            title="strikethrough"
-            handleClick={DOMEventHandlers.handleStrikeout}
-          >
-            <StrikethroughSIcon />
-          </ButtonWrapper>}
-          {(!_props?.toolbar || _props?.toolbar?.leftAlign) && <ButtonWrapper
-            sx={_props?.toolbarClass?.item?.leftAlign} 
-            title="left align"
-            handleClick={() => DOMEventHandlers.handleAlign(RowFlex.LEFT)}
-          >
-            <FormatAlignLeftIcon />
-          </ButtonWrapper>}
-          {(!_props?.toolbar || _props?.toolbar?.centerAlign) && <ButtonWrapper
-            sx={_props?.toolbarClass?.item?.centerAlign} 
-            title="center align"
-            handleClick={() => DOMEventHandlers.handleAlign(RowFlex.CENTER)}
-          >
-            <FormatAlignCenterIcon />
-          </ButtonWrapper>}
-          {(!_props?.toolbar || _props?.toolbar?.rightAlign) && <ButtonWrapper
-            sx={_props?.toolbarClass?.item?.rightAlign} 
-            title="right align"
-            handleClick={() => DOMEventHandlers.handleAlign(RowFlex.RIGHT)}
-          >
-            <FormatAlignRightIcon />
-          </ButtonWrapper>}
-          {(!_props?.toolbar || _props?.toolbar?.justify) && <ButtonWrapper
-            sx={_props?.toolbarClass?.item?.justify} 
-            title="justify"
-            handleClick={() => DOMEventHandlers.handleAlign(RowFlex.ALIGNMENT)}
-          >
-            <FormatAlignJustifyIcon />
-          </ButtonWrapper>}
-          {(!_props?.toolbar || _props?.toolbar?.bulletList) && <ButtonWrapper
-            sx={_props?.toolbarClass?.item?.bulletList} 
-            title="bullet list"
-            handleClick={() =>
-              DOMEventHandlers.handleList(ListType.UL, ListStyle.DECIMAL)
-            }
-          >
-            <FormatListBulletedIcon />
-          </ButtonWrapper>}
-          {(!_props?.toolbar || _props?.toolbar?.numberedList) && <ButtonWrapper
-            sx={_props?.toolbarClass?.item?.numberedList} 
-            title="numbered list"
-            handleClick={() =>
-              DOMEventHandlers.handleList(ListType.OL, ListStyle.DECIMAL)
-            }
-          >
-            <FormatListNumberedIcon />
-          </ButtonWrapper>}
-          {(!_props?.toolbar || _props?.toolbar?.fontType) && <FontDropdown {...({style: _props?.toolbarClass?.item?.fontType} as any)} />}
-          {(!_props?.toolbar || _props?.toolbar?.table) && <TableDropdown {...({style: _props?.toolbarClass?.item?.table} as any)} />}
-          {(!_props?.toolbar || _props?.toolbar?.fontColor) && <FontColorButton style={_props?.toolbarClass?.item?.fontColor} textColor={contentStyles?.color} />}
-          {(!_props?.toolbar || _props?.toolbar?.highlight) && <HighlightTextButton style={_props?.toolbarClass?.item?.highlight} color={contentStyles?.highlight} />}
-          {(!_props?.toolbar || _props?.toolbar?.fontSize) && <FontSizeButton style={_props?.toolbarClass?.item?.fontSize} size={contentStyles?.size} />}
-          {(!_props?.toolbar || _props?.toolbar?.heading) && <HeadingButton {...({style: _props?.toolbarClass?.item?.heading} as any)} />}
-          {(!_props?.toolbar || _props?.toolbar?.hyperlink) && <ButtonWrapper
-            sx={_props?.toolbarClass?.item?.hyperlink} 
-            title="hyperlink"
-            handleClick={() => DOMEventHandlers.createHyperLink()}
-          >
-            <InsertLinkIcon />
-          </ButtonWrapper>}
-          {(!_props?.toolbar || _props?.toolbar?.image) && <ImageUploadButton {...({style: _props?.toolbarClass?.item?.image} as any)} />}
+          {(!_props?.toolbar || _props?.toolbar?.bold) && (
+            <ButtonWrapper
+              sx={_props?.toolbarClass?.item?.bold}
+              title='Bold'
+              handleClick={DOMEventHandlers.handleBold}>
+              <FormatBoldIcon style={{ fontSize: "large" }} />
+            </ButtonWrapper>
+          )}
+          {(!_props?.toolbar || _props?.toolbar?.italic) && (
+            <ButtonWrapper
+              sx={_props?.toolbarClass?.item?.italic}
+              title='Italic'
+              handleClick={DOMEventHandlers.handleItalic}>
+              <FormatItalicIcon style={{ fontSize: "large" }} />
+            </ButtonWrapper>
+          )}
+          {(!_props?.toolbar || _props?.toolbar?.underline) && (
+            <ButtonWrapper
+              sx={_props?.toolbarClass?.item?.underline}
+              title='Underline'
+              handleClick={DOMEventHandlers.handleUnderline}>
+              <FormatUnderlinedIcon style={{ fontSize: "large" }} />
+            </ButtonWrapper>
+          )}
+          <Divider
+            flexItem
+            orientation='vertical'
+            sx={{ mx: 0.5, my: 1 }}
+            style={{ marginLeft: -5, marginRight: 5 }}
+          />
+
+          {(!_props?.toolbar || _props?.toolbar?.subscript) && (
+            <ButtonWrapper
+              sx={_props?.toolbarClass?.item?.subscript}
+              title='Subscript'
+              handleClick={DOMEventHandlers.handleSubscript}>
+              <SubscriptIcon style={{ fontSize: "large" }} />
+            </ButtonWrapper>
+          )}
+          {(!_props?.toolbar || _props?.toolbar?.superscript) && (
+            <ButtonWrapper
+              sx={_props?.toolbarClass?.item?.superscript}
+              title='Superscript'
+              handleClick={DOMEventHandlers.handleSuperscript}>
+              <SuperscriptIcon style={{ fontSize: "large" }} />
+            </ButtonWrapper>
+          )}
+          {(!_props?.toolbar || _props?.toolbar?.strikethrough) && (
+            <ButtonWrapper
+              sx={_props?.toolbarClass?.item?.strikethrough}
+              title='Strikethrough'
+              handleClick={DOMEventHandlers.handleStrikeout}>
+              <StrikethroughSIcon style={{ fontSize: "large" }} />
+            </ButtonWrapper>
+          )}
+          <Divider
+            flexItem
+            orientation='vertical'
+            sx={{ mx: 0.5, my: 1 }}
+            style={{ marginLeft: -5, marginRight: 5 }}
+          />
+
+          {(!_props?.toolbar || _props?.toolbar?.leftAlign) && (
+            <ButtonWrapper
+              sx={_props?.toolbarClass?.item?.leftAlign}
+              title='Left align'
+              handleClick={() => DOMEventHandlers.handleAlign(RowFlex.LEFT)}>
+              <FormatAlignLeftIcon style={{ fontSize: "large" }} />
+            </ButtonWrapper>
+          )}
+          {(!_props?.toolbar || _props?.toolbar?.centerAlign) && (
+            <ButtonWrapper
+              sx={_props?.toolbarClass?.item?.centerAlign}
+              title='Center align'
+              handleClick={() => DOMEventHandlers.handleAlign(RowFlex.CENTER)}>
+              <FormatAlignCenterIcon style={{ fontSize: "large" }} />
+            </ButtonWrapper>
+          )}
+          {(!_props?.toolbar || _props?.toolbar?.rightAlign) && (
+            <ButtonWrapper
+              sx={_props?.toolbarClass?.item?.rightAlign}
+              title='Right align'
+              handleClick={() => DOMEventHandlers.handleAlign(RowFlex.RIGHT)}>
+              <FormatAlignRightIcon style={{ fontSize: "large" }} />
+            </ButtonWrapper>
+          )}
+          {(!_props?.toolbar || _props?.toolbar?.justify) && (
+            <ButtonWrapper
+              sx={_props?.toolbarClass?.item?.justify}
+              title='Justify'
+              handleClick={() =>
+                DOMEventHandlers.handleAlign(RowFlex.ALIGNMENT)
+              }>
+              <FormatAlignJustifyIcon style={{ fontSize: "large" }} />
+            </ButtonWrapper>
+          )}
+          {(!_props?.toolbar || _props?.toolbar?.bulletList) && (
+            <ButtonWrapper
+              sx={_props?.toolbarClass?.item?.bulletList}
+              title='Bullet list'
+              handleClick={() =>
+                DOMEventHandlers.handleList(ListType.UL, ListStyle.DECIMAL)
+              }>
+              <FormatListBulletedIcon style={{ fontSize: "large" }} />
+            </ButtonWrapper>
+          )}
+          {(!_props?.toolbar || _props?.toolbar?.numberedList) && (
+            <ButtonWrapper
+              sx={_props?.toolbarClass?.item?.numberedList}
+              title='Numbered list'
+              handleClick={() =>
+                DOMEventHandlers.handleList(ListType.OL, ListStyle.DECIMAL)
+              }>
+              <FormatListNumberedIcon style={{ fontSize: "large" }} />
+            </ButtonWrapper>
+          )}
+          <Divider
+            flexItem
+            orientation='vertical'
+            sx={{ mx: 0.5, my: 1 }}
+            style={{ marginLeft: -5, marginRight: 5 }}
+          />
+
+          {(!_props?.toolbar || _props?.toolbar?.fontType) && (
+            <FontDropdown
+              {...({ style: _props?.toolbarClass?.item?.fontType } as any)}
+            />
+          )}
+          <Divider
+            flexItem
+            orientation='vertical'
+            sx={{ mx: 0.5, my: 1 }}
+            style={{ marginLeft: 5, marginRight: 5 }}
+          />
+
+          {(!_props?.toolbar || _props?.toolbar?.table) && (
+            <TableDropdown
+              {...({ style: _props?.toolbarClass?.item?.table } as any)}
+            />
+          )}
+          {(!_props?.toolbar || _props?.toolbar?.fontColor) && (
+            <FontColorButton
+              style={_props?.toolbarClass?.item?.fontColor}
+              textColor={contentStyles?.color}
+            />
+          )}
+          {(!_props?.toolbar || _props?.toolbar?.highlight) && (
+            <HighlightTextButton
+              style={_props?.toolbarClass?.item?.highlight}
+              color={contentStyles?.highlight}
+            />
+          )}
+          <Divider
+            flexItem
+            orientation='vertical'
+            sx={{ mx: 0.5, my: 1 }}
+            style={{ marginLeft: -5, marginRight: 5 }}
+          />
+
+          {(!_props?.toolbar || _props?.toolbar?.fontSize) && (
+            <FontSizeButton
+              style={_props?.toolbarClass?.item?.fontSize}
+              size={contentStyles?.size}
+            />
+          )}
+          <Divider
+            flexItem
+            orientation='vertical'
+            sx={{ mx: 0.5, my: 1 }}
+            style={{ marginLeft: 5, marginRight: 5 }}
+          />
+
+          {(!_props?.toolbar || _props?.toolbar?.heading) && (
+            <HeadingButton
+              {...({ style: _props?.toolbarClass?.item?.heading } as any)}
+            />
+          )}
+          <Divider
+            flexItem
+            orientation='vertical'
+            sx={{ mx: 0.5, my: 1 }}
+            style={{ marginLeft: -5, marginRight: 5 }}
+          />
+
+          {(!_props?.toolbar || _props?.toolbar?.hyperlink) && (
+            <ButtonWrapper
+              sx={_props?.toolbarClass?.item?.hyperlink}
+              title='Hyperlink'
+              handleClick={() => DOMEventHandlers.createHyperLink()}>
+              <InsertLinkIcon style={{ fontSize: "large" }} />
+            </ButtonWrapper>
+          )}
+          {(!_props?.toolbar || _props?.toolbar?.image) && (
+            <ImageUploadButton
+              {...({ style: _props?.toolbarClass?.item?.image } as any)}
+            />
+          )}
         </Stack>
       </AppBar>
     </Box>
