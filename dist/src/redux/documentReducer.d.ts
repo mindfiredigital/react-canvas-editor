83046e6dc2475c3cff136c45a1e26cd3804eb0ce
Ashutosh8215 <ashutosh.j@mindfiresolutions.com>
2024-03-01 12:35:26 +0530

    chore: remove folder name

--- dist/src/redux/documentReducer.d.ts
+++
@@ -1,9 +0,0 @@
-export interface DocumentState {
-    documentId: string;
-    title: string;
-    reloadRecentDoc: boolean;
-    margins: number[];
-}
-export declare const setDocumentTitle: import("@reduxjs/toolkit").ActionCreatorWithPayload<any, "document/setDocumentTitle">, reloadRecentDoc: import("@reduxjs/toolkit").ActionCreatorWithPayload<any, "document/reloadRecentDoc">, setDocumentId: import("@reduxjs/toolkit").ActionCreatorWithPayload<any, "document/setDocumentId">, setDocumentMargins: import("@reduxjs/toolkit").ActionCreatorWithPayload<any, "document/setDocumentMargins">;
-declare const _default: import("redux").Reducer<DocumentState, import("redux").AnyAction>;
-export default _default;
